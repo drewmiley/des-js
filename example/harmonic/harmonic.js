cb54dbf14a409dea7f31f690800b06321846f239
Drew Miley <dmiley@scottlogic.co.uk>
2016-02-10 14:52:02 +0000

    Adds dummy chart to harmonic example

--- example/harmonic/harmonic.js
+++ example/harmonic/harmonic.js
@@ -1,5 +1,43 @@
 (function(d3, fc, des) {
     'use strict';
 
-    console.log(des);
+    var sinData = d3.range(50).map(function(d) {
+        return {
+            x: d,
+            y: Math.sin(d)
+        };
+    });
+
+    // create a chart
+    var sinChart = fc.chart.cartesian(
+                  d3.scale.linear(),
+                  d3.scale.linear())
+              .yDomain(fc.util.extent().pad(0.2).fields(["y"])(sinData))
+              .yLabel("Sine")
+              .yNice()
+              .yOrient("left")
+              .xDomain(fc.util.extent().fields("x")(sinData))
+              .xLabel("Time")
+              .xBaseline(0)
+              .chartLabel("Sine Chart")
+              .margin({left: 50, bottom: 20, top: 30});
+
+    // create a pair of series and some gridlines
+    var sinLine = fc.series.line()
+      .xValue(function(d) { return d.x; })
+      .yValue(function(d) { return d.y; });
+
+    var gridlines = fc.annotation.gridline();
+
+    // combine using a multi-series
+    var multi = fc.series.multi()
+      .series([gridlines, sinLine]);
+
+    sinChart.plotArea(multi);
+
+    // render
+    d3.select("#sin-chart")
+        .datum(sinData)
+        .call(sinChart);
+
 })(d3, fc, des);
