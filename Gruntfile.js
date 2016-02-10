d9e7a63e79c3307a91d7ba21f36d7edeff36526d
Drew Miley <dmiley@scottlogic.co.uk>
2016-02-10 11:59:09 +0000

    Rework of project structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,88 @@
+/* global module, require */
+
 module.exports = function(grunt) {
+    'use strict';
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-      options: {
-        preserveComments: 'some'
-      },
+        meta: {
+            srcFiles: [
+                'src/**/*'
+            ],
+            srcJsFiles: [
+                'src/des.js'
+            ],
+            ourJsFiles: [
+                'Gruntfile.js',
+                '<%= meta.srcJsFiles %>'
+            ],
+            coverageDir: 'coverage'
+        },
 
-      build: {
-        src: 'src/des-js.js',
-        dest: 'dist/des-js.min.js'
-      }
-    }
-  });
+        jscs: {
+            options: {
+                config: '.jscsrc'
+            },
+            failOnError: {
+                files: {
+                    src: ['<%= meta.ourJsFiles %>']
+                }
+            },
+            warnOnly: {
+                options: {
+                    force: true
+                },
+                files: {
+                    src: ['<%= meta.ourJsFiles %>']
+                }
+            }
+        },
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            failOnError: {
+                files: {
+                    src: ['<%= meta.ourJsFiles %>']
+                }
+            },
+            warnOnly: {
+                options: {
+                    force: true
+                },
+                files: {
+                    src: ['<%= meta.ourJsFiles %>']
+                }
+            }
+        },
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+        concat: {
+            development: {
+                src: ['<%= meta.srcJsFiles %>'],
+                dest: 'dist/des-js.min.js',
+                options: {
+                    sourceMap: true
+                }
+            },
+            production: {
+                src: ['<%= meta.srcJsFiles %>'],
+                dest: 'dist/des-js.min.js',
+                options: {
+                    sourceMap: false
+                }
+            }
+        }
+
+    });
+
+    require('load-grunt-tasks')(grunt, {
+        pattern: ['grunt-*']
+    });
+
+    grunt.registerTask('default', ['build']);
+
+    grunt.registerTask('build', ['concat:production']);
 
 };
