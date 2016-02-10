2dffe567a8e599b64d8fadd765753dad2cc67724
Drew Miley <dmiley@scottlogic.co.uk>
2016-02-10 12:00:38 +0000

    Initial global namespace

--- src/des.js
+++ src/des.js
@@ -1,1 +1,12 @@
-var i = 0;
+(function() {
+    'use strict';
+
+    // Crazyness to get a strict mode compliant reference to the global object
+    var global = null;
+    /* jshint ignore:start */
+    global = (1, eval)('this');
+    /* jshint ignore:end */
+
+    global.des = {
+    };
+})();
